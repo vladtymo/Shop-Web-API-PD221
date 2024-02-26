82dcd24bd330e542cd61c547e12f4b88569c55e6
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-02-26 17:13:43 +0200

    TODO: reset pass

--- BusinessLogic/Interfaces/IAccountsService.cs
+++ BusinessLogic/Interfaces/IAccountsService.cs
@@ -7,5 +7,18 @@ namespace Core.Interfaces
         Task Register(RegisterModel model);
         Task Login(LoginModel model);
         Task Logout();
+        //Task<ResetPasswordResponse> ResetPasswordRequest(string email);
+        //Task ResetPassword(ResetPasswordModel model);
+    }
+
+    // TODO: implement reset user password endpoints
+    public class ResetPasswordResponse
+    {
+        public string Token { get; set; }
+    }
+    public class ResetPasswordModel
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
     }
 }
